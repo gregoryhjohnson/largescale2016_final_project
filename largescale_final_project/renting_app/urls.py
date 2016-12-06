f3f835e74e3972f3a67500f7eb97362516dc3775
Dana Kirsanov <dk2425@nyu.edu>
2016-12-06 00:28:29 -0500

    added a bunch of views

--- largescale_final_project/renting_app/urls.py
+++ largescale_final_project/renting_app/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
   url(r'^register/$', views.register, name='register'),
   url(r'^login/$', views.user_login, name='login'),
   url(r'^logout/$', views.user_logout, name='logout'),
+  url(r'^profile/(?P<user_id>[0-9]+)/$', views.profile, name='profile'),
+  url(r'^add-item/$', views.add, name='add-item'),
+  url(r'^home/$', views.home, name='home'),
+  url(r'^item/(?P<item_id>[0-9]+)', views.item, name='item'),
+  url(r'^modify/$', views.modify, name='modify'),
 ]
